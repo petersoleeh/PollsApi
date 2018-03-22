d978516ebe8ecdb026cd80c7172b9c2e36c93175
peter soleeh <petersoleeh@gmail.com>
2018-03-22 12:36:55 +0300

    /polls  /polls/1

---
+++ polls/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, re_path
+from . import views
+from .views import polls_details, polls_list
+
+urlpatterns = [
+    path('polls/', polls_list,name='polls_list'),
+    path('polls/<int:pk>/', polls_details, name='polls_details')
+
+
+]
