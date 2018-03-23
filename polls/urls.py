a4e1d5460c36ddb822b89d5bae11697580b3797d
peter soleeh <petersoleeh@gmail.com>
2018-03-23 15:30:23 +0300

    landing page urls

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,7 @@ from . import views
 from .views import polls_details, polls_list
 
 urlpatterns = [
+    path(r'^$', views.index,name='index'),
     path('polls/', polls_list,name='polls_list'),
     path('polls/<int:pk>/', polls_details, name='polls_details')
 
